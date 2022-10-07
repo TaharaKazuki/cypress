e38bccdcd3e0916f9ab01460c9f3aa60771fa583
Tahara Kazuki <ssp0727.lnc@gmail.com>
2022-10-07 22:38:43 +0900

    update

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Image from 'next/image'
+import { SearchIcon, PlusCircleIcon } from '@heroicons/react/outline'
+import { HomeIcon } from '@heroicons/react/solid'
 
 const Header = () => {
   return (
     <div className="flex max-w-6xl items-center justify-between">
       <div className="relative hidden h-24 w-24 cursor-pointer lg:inline-grid">
         <Image
-          src="https://fontmeme.com/temporary/caa6aab112937f05a3ffde3c78dbb602.png"
+          src="https://fontmeme.com/permalink/221007/5e91c376f90ff4cdd6b64f3c9b91b945.png"
           layout="fill"
           className="object-contain"
           alt="tahastagram"
@@ -20,6 +22,18 @@ const Header = () => {
           alt="logo"
         />
       </div>
+
+      <div className="relative mt-1">
+        <div className="absolute top-2 left-2">
+          <SearchIcon className="h-5 text-gray-500" />
+        </div>
+        <input
+          type="text"
+          placeholder="Search"
+          className="rounded-md border-gray-500 bg-gray-50 pl-10 text-sm focus:border-black focus:ring-black"
+        />
+      </div>
+
       <h1>Right sides</h1>
     </div>
   )
